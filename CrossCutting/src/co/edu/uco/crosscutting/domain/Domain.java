85a106191be581689a0ed29b02ff6f9dc297435e
Juan6360 <162759252+Juan6360@users.noreply.github.com>
2024-10-02 00:46:25 -0500

    Up To Date

---
+++ CrossCutting/src/co/edu/uco/crosscutting/domain/Domain.java
@@ -0,0 +1,24 @@
+package co.edu.uco.crosscutting.domain;
+
+import java.util.UUID;
+
+import co.edu.uco.crosscutting.helpers.TextHelper;
+import co.edu.uco.crosscutting.helpers.UUIDHelper;
+
+class Domain {
+	
+	private UUID id;
+	
+	protected Domain(final UUID id) {
+		setId(id);
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(final UUID id) {
+		this.id = UUIDHelper.getDefault(id, UUIDHelper.getDefault());
+	}
+	
+}
